b43a812831ca7536aee85b9f25d6576a02c18aa4
Khizar723 <Khizar723@users.noreply.github.com>
2019-12-17 11:45:08 -0500

    hi

--- public/index.js
+++ public/index.js
@@ -2,8 +2,18 @@ const path = require("path");
 const express = require("express")
 const app = express();
 
-app.get("", (req, res) => {
-    res.send("Thami likes bagels and butter. Also he smells")
+//define paths for express
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views")
+//setup hanlebars engine 
+app.set("viws engine", "hbs")//Telling express/NODE to use handlebars for templates
+app.set("viws", viewsPath)//telling express to get templates from templates/views folder
+app.get("", async (req, res) => {
+    try {
+        res.render("index")
+    } catch (error) {
+        res.status(500).send()
+    }
 })
 
 app.get("/thamisucks", (req, res) => {
@@ -17,4 +27,5 @@ app.get("/thamisucks", (req, res) => {
 
 app.listen(3000, (req, res)=> {
     console.log('listening on port 3000')
+    console.log(viewsPath)
 })
