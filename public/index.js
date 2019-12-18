b2fe2a271acda8df3143ca96b86ec1160d5be6b2
Khizar723 <Khizar723@users.noreply.github.com>
2019-12-18 11:43:50 -0500

    hi

--- public/index.js
+++ public/index.js
@@ -1,17 +1,51 @@
 const path = require("path");
 const express = require("express")
 const app = express();
+const hbs = require("hbs")
 
 //define paths for express
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
 //setup hanlebars engine 
-app.set("viws engine", "hbs")//Telling express/NODE to use handlebars for templates
-app.set("viws", viewsPath)//telling express to get templates from templates/views folder
+app.set("view engine", "hbs")//Telling express/NODE to use handlebars for templates
+app.set("views", viewsPath)//telling express to get templates from templates/views folder
+hbs.registerPartials(partialsPath)
 app.get("", async (req, res) => {
     try {
-        res.render("index")
-    } catch (error) {
+        res.render("index", {
+            title: "Our First Express App"
+        })
+    } catch {
+        res.status(500).send()
+    }
+})
+
+app.get("/thamisucks", (req, res) => {
+    res.send("Thami Sucks")
+})
+
+
+app.get("/about/:id", async (req, res) => {
+    const name = req.params.id
+    try {
+        res.render("index", {
+            title: `${name}`
+        })
+    } catch {
+        res.status(500).send()
+    }
+})
+
+app.get("/thamisucks", (req, res) => {
+    res.send("Thami Sucks")
+})
+
+app.get("/swag", async (req, res) => {
+    const name = req.params.id
+    try {
+        res.render("swag")
+    } catch {
         res.status(500).send()
     }
 })
